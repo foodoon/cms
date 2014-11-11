9dd2119bd30ceb1be89c131ae4b6f7a8eddded31
foodoo <foodoon@qq.com>
2014-11-11 09:13:47 +0800

    add

--- app/cms-web/src/main/java/guda/cms/action/admin/CmsLoginAct.java
+++ app/cms-web/src/main/java/guda/cms/action/admin/CmsLoginAct.java
@@ -4,6 +4,8 @@ import static org.apache.shiro.web.filter.authc.FormAuthenticationFilter.DEFAULT
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import guda.common.web.CookieUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,20 +14,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CmsLoginAct {
 
-	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
-	public String input(HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		return "login";
-	}
+    public static final String SITE_COOKIE = "_site_id_cookie";
 
-	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
-	public String submit(String username, String password, String captcha,
-			String message, HttpServletRequest request,
-			HttpServletResponse response, ModelMap model) {
-		Object error = request.getAttribute(DEFAULT_ERROR_KEY_ATTRIBUTE_NAME);
-		if (error != null) {
-			model.addAttribute("error", error);
-		}
-		return "login";
-	}
+
+    @RequestMapping(value = "/login.do", method = RequestMethod.GET)
+    public String input(HttpServletRequest request,
+            HttpServletResponse response, ModelMap model) {
+        //可能进入没有权限子站(cookie记录站点)需要清除cookie
+        CookieUtils.cancleCookie(request, response, SITE_COOKIE, null);
+        return "login";
+    }
+
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST)
+    public String submit(String username, String password, String captcha,
+            String message, HttpServletRequest request,
+            HttpServletResponse response, ModelMap model) {
+        Object error = request.getAttribute(DEFAULT_ERROR_KEY_ATTRIBUTE_NAME);
+        if (error != null) {
+            model.addAttribute("error", error);
+        }
+        return "login";
+    }
+
 }
