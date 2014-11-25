50499b8dc4369219e820f948d1657dc88c10edd6
well <foodoon@qq.com>
2014-11-25 14:05:57 +0800

    add

---
+++ app/cms-web/src/main/java/guda/cms/service/WebServer.java
@@ -0,0 +1,17 @@
+package guda.cms.service;
+
+/**
+ * Created by well on 2014/11/25.
+ */
+public class WebServer {
+
+    private String pageServer;
+
+    public String getPageServer() {
+        return pageServer;
+    }
+
+    public void setPageServer(String pageServer) {
+        this.pageServer = pageServer;
+    }
+}
