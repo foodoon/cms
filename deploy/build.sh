b1e047de513782272551027e81f709046da1ad2c
well <foodoon@qq.com>
2014-11-26 14:14:55 +0800

    add

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,6 +1,6 @@
 cd .. && git pull
 tomcat_home=/usr/tomcat-news
-mvn package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 mv assembly/target/cms-1.0-SNAPSHOT.war ROOT.war
 mv ${tomcat_home}/webapps/ROOT.war ${tomcat_home}/webapps/ROOT.war.bak
 rm -rf ${tomcat_home}/webapps/ROOT
