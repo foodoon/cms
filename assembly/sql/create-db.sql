93b368145cc2171b8d923ac9754e9c010ae501b4
gang <foodoon@qq.com>
2014-12-20 20:57:49 +0800

    add

--- assembly/sql/create-db.sql
+++ assembly/sql/create-db.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS `cmstest`;
-CREATE DATABASE `cmstest` default charset=utf8 ;
-use `cmstest`;
+DROP DATABASE IF EXISTS `cms`;
+CREATE DATABASE `cms` default charset=utf8 ;
+use `cms`;
 
 
-delete from mysql.user where User = 'cmstest_user';
-grant select,update,delete,insert on `cmstest`.* to 'cmstest_user'@'%' identified by 'cms_pwd';
-grant select,update,delete,insert on `cmstest`.* to 'cmstest_user'@'localhost' identified by 'cms_pwd';
+delete from mysql.user where User = 'cms_user';
+grant select,update,delete,insert on `cms`.* to 'cms_user'@'%' identified by 'cms_pwd';
+grant select,update,delete,insert on `cms`.* to 'cms_user'@'localhost' identified by 'cms_pwd';
 flush privileges;
